2948b093b1713d72f0f255fb9e41c2a35f727d89
Audun Nes <audun.nes@gmail.com>
2024-02-06 12:57:57 +0100

    Add output for returning the id of a newly created folder

---
+++ grafana_folder/outputs.tf
@@ -0,0 +1,3 @@
+output "grafana_folder_id" {
+  value = grafana_folder.this.id
+}
--- grafana_folder/variables.tf
+++ grafana_folder/variables.tf
@@ -1,10 +1,10 @@
 variable "title" {
   type        = string
-  description = "The folder title. (Mandatory)."
+  description = "The folder title."
 }
 
 variable "uid" {
   type        = string
   default     = null
-  description = "The uid for the folder. If omitted. We will generate a random value. (Optional)."
+  description = "The uid for the folder. If omitted. We will generate a random value."
 }
