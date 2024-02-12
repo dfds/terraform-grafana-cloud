4f5d6f061696fc9266448deb2b938cd1fd334be6
samidbb <samdi@dfds.com>
2024-02-12 12:41:46 +0100

    support for reading interval from string

--- grafana_alert/main.tf
+++ grafana_alert/main.tf
@@ -1,11 +1,11 @@
 
 resource "grafana_rule_group" "alertrule" {
-  for_each   = { for file in var.alertrule_files : file => jsondecode(file("${file}")) }
+  for_each = { for file in var.alertrule_files : file => jsondecode(file("${file}"))
+  }
   name       = each.value.groups[0].name
-  folder_uid = var.folder
-  #   interval_seconds = each.value.groups[0].interval # Cannot read string property "1m"
-  interval_seconds = 60
-
+  folder_uid = var.folder # Folder specified in the json will be skipped since it does not contain UID.
+  # This solve the example for having to convert "1m" to integer seconds
+  interval_seconds = regex("^[0-9]*", each.value.groups[0].interval) * (regex("[m|h|s]$", each.value.groups[0].interval) == "m" ? 60 : (regex("[m|h|s]$", each.value.groups[0].interval) == "h" ? 1440 : 1))
   dynamic "rule" {
     for_each = each.value.groups[0].rules
     content {
