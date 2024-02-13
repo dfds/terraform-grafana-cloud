2cf9d2f774289db6788c8a01e83319b0612687ca
samidbb <samdi@dfds.com>
2024-02-13 10:25:00 +0100

    vars types added

--- grafana_alert/variables.tf
+++ grafana_alert/variables.tf
@@ -1,7 +1,9 @@
 variable "folder" {
   description = "UID of the folder to create the alert rules in"
+  type        = string
 }
 
 variable "alertrule_files" {
   description = "Path to the json files with alert rules"
+  type        = list(string)
 }
