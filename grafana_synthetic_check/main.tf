5ffec8bb51f7a4dad65587ad5e61e38754e8f702
Audun Nes <aunes@dfds.com>
2024-02-20 15:23:21 +0100

    Add support for synthetic monitoring

--- grafana_synthetic_check/main.tf
+++ grafana_synthetic_check/main.tf
@@ -1,36 +1,33 @@
 
 data "grafana_synthetic_monitoring_probes" "this" {}
 
-
 resource "grafana_synthetic_monitoring_check" "http" {
-
-  job     = var.job
-  target  = var.target
-  enabled = var.is_enabled
+  for_each = { for file in var.synthetic_files : file => jsondecode(file(file))
+  }
+  job       = each.value.job
+  target    = each.value.target
+  enabled   = try(each.value.enabled, true)
+  frequency = try(each.value.frequency, 60000)
+  timeout   = try(each.value.timeout, 3000)
   probes = [
     data.grafana_synthetic_monitoring_probes.this.probes.Frankfurt,
     data.grafana_synthetic_monitoring_probes.this.probes.London,
   ]
-  labels = var.labels
+  labels = try(each.value.labels, {})
 
   settings {
     http {
-      method = upper(var.http_check_settings.method)
-      dynamic "bearer_token" {
-        for_each = var.http_check_settings.bearer_token != null ? [var.http_check_settings.bearer_token] : []
-        content {
-          token = var.http_check_settings.bearer_token.token
-        }
-      }
+      method       = upper(try(each.value.settings.method, "GET"))
+      bearer_token = try(each.value.settings.bearer_token, null)
       dynamic "basic_auth" {
-        for_each = var.http_check_settings.basic_auth != null ? [var.http_check_settings.basic_auth] : []
+        for_each = try([each.value.settings.basic_auth], [])
         content {
-          username = var.http_check_settings.basic_auth.username
-          password = var.http_check_settings.basic_auth.password
+          username = try(each.value.settings.basic_auth.username, null)
+          password = try(each.value.settings.basic_auth.password, null)
         }
       }
-      valid_status_codes  = var.http_check_settings.valid_status_codes
-      no_follow_redirects = var.http_check_settings.no_follow_redirects
+      valid_status_codes  = try(each.value.settings.valid_status_codes, [200])
+      no_follow_redirects = try(each.value.settings.no_follow_redirects, false)
     }
   }
 }
--- grafana_synthetic_check/variables.tf
+++ grafana_synthetic_check/variables.tf
@@ -1,36 +1,9 @@
-variable "job" {
-  description = "The job name to associate the check with"
-  type        = string
-}
-
-variable "target" {
-  description = "The target to check"
-  type        = string
-}
-
-variable "is_enabled" {
-  description = "Whether the check is enabled"
-  type        = bool
-  default     = false
-}
-
-variable "labels" {
-  description = "A map of labels to associate with the check"
-  type        = map(string)
-  default     = {}
-}
-
-variable "http_check_settings" {
-  type = object({
-    method = string                  // GET, POST, PUT, DELETE, HEAD, OPTIONS, PATCH
-    bearer_token = optional(object({ # Optional
-      token = string
-    }), null)
-    basic_auth = optional(object({ # Optional
-      username = string
-      password = string
-    }), null)
-    valid_status_codes  = list(number)
-    no_follow_redirects = optional(bool, false)
-  })
+variable "synthetic_files" {
+  description = <<EOF
+    Path to the json files with synthetic monitoring targets.
+    The files must be compliant with the /api/v1/check/add method in
+    https://github.com/grafana/synthetic-monitoring-api-go-client/blob/main/docs/API.md
+  EOF
+  type        = list(string)
+  default     = []
 }
