9575b760644b997a7f9555cf0127af13a2a6201d
Aleksandra Fromm <alekorc@dfds.com>
2024-04-16 11:52:08 +0200

    Format

--- versions.tf
+++ versions.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = ">= 5.34.0"
+      source                = "hashicorp/aws"
+      version               = ">= 5.34.0"
       configuration_aliases = [aws.route53]
     }
     grafana = {
-      source = "grafana/grafana"
-      version = ">= 2.9.0"
+      source                = "grafana/grafana"
+      version               = ">= 2.9.0"
       configuration_aliases = [grafana.cloud]
     }
   }
