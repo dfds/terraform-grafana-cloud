347196921bbc141e02d526b7ea6660320873fd99
Aleksandra Fromm <alekorc@dfds.com>
2024-05-13 09:58:04 +0200

    Add grafana_data_source submodule

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,12 @@ output "read_only_multi_stack_token" {
   value = grafana_cloud_access_policy_token.read_only_multi_stack[*].token
   sensitive = true
 }
+
+output "grafana_url" {
+  value = grafana_cloud_stack.this.url
+}
+
+output "terraform_sa_token" {
+  value = grafana_cloud_stack_service_account_token.this.key
+  sensitive = true
+}
