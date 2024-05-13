6057afe33eaea8e1bb22c3a87daabcc70acdb250
Aleksandra Fromm <61739075+aleksandra-fromm@users.noreply.github.com>
2024-05-13 11:21:07 +0200

    Add grafana_data_source submodule (#32)
    * Add grafana_data_source submodule
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ grafana_data_source/main.tf
@@ -0,0 +1,11 @@
+resource "grafana_data_source" "this" {
+  for_each = { for data_source in var.data_sources : format("%s-%s", data_source.type, data_source.name) => data_source }
+
+  name                     = each.value.name
+  type                     = each.value.type
+  url                      = lookup(each.value, "url", "")
+  basic_auth_enabled       = lookup(each.value, "basic_auth_enabled", false)
+  basic_auth_username      = lookup(each.value, "basic_auth_username", "")
+  json_data_encoded        = lookup(each.value, "json_data_encoded", "")
+  secure_json_data_encoded = lookup(each.value, "secure_json_data_encoded", "")
+}
---
+++ grafana_data_source/variables.tf
@@ -0,0 +1,12 @@
+variable "data_sources" {
+  type = list(object({
+    name                     = string
+    type                     = string
+    url                      = optional(string)
+    basic_auth_enabled       = optional(bool)
+    basic_auth_username      = optional(string)
+    json_data_encoded        = optional(string)
+    secure_json_data_encoded = optional(string)
+  }))
+  description = "List of data sources"
+}
