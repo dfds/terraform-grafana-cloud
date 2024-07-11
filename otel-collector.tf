9f7f8d99910334a9e825dacb00ded431ac0deb12
Sami <40062553+samidbb@users.noreply.github.com>
2024-07-11 13:37:05 +0200

    collector svc name based on cname. resource label added (#50)

--- otel-collector.tf
+++ otel-collector.tf
@@ -1,21 +1,22 @@
 locals {
-    otlp_auth_header = var.create_write_only_token ? base64encode("${grafana_cloud_stack.this.id}:${grafana_cloud_access_policy_token.write_only[0].token}") : ""
+  otlp_auth_header = var.create_write_only_token ? base64encode("${grafana_cloud_stack.this.id}:${grafana_cloud_access_policy_token.write_only[0].token}") : ""
 }
 
 resource "helm_release" "otel_collector" {
-    count = var.deploy_otel_agent_k8s ? 1 : 0
-    name = "otel-collector-${var.slug}"
-    namespace = var.otel_collector_namespace
-    repository = "https://open-telemetry.github.io/opentelemetry-helm-charts"
-    chart = "opentelemetry-collector"
-    version = var.otel_collector_chart_version
+  count      = var.deploy_otel_agent_k8s ? 1 : 0
+  name       = "otel-collector-${var.slug}"
+  namespace  = var.otel_collector_namespace
+  repository = "https://open-telemetry.github.io/opentelemetry-helm-charts"
+  chart      = "opentelemetry-collector"
+  version    = var.otel_collector_chart_version
 
-    values = [
-        templatefile("${path.module}/otel-collector/values.yaml", {
-            otlphttp_endpoint = "${grafana_cloud_stack.this.otlp_url}/otlp"
-            stack_token = local.otlp_auth_header
-            name = "otel-${var.slug}"
-            owner = "CloudEngineering"
-        })
-    ]
+  values = [
+    templatefile("${path.module}/otel-collector/values.yaml", {
+      otlphttp_endpoint = "${grafana_cloud_stack.this.otlp_url}/otlp"
+      stack_token       = local.otlp_auth_header
+      name              = "otel-${var.route53_record_name}"
+      owner             = "CloudEngineering"
+      stack             = var.slug
+    })
+  ]
 }
